8abf3cfd23cd47c4ec7856eb6c1210ff6749bd52
Jiggins242 <44429273+Jiggins242@users.noreply.github.com>
2019-01-04 12:10:14 +0000

    Basic query and mutation added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # GraphQl-Login-Academind
 
 ## Fire Up the engines 
+
+To start the server. In root folder:
+
+npm start
--- app.js
+++ app.js
@@ -1,12 +1,86 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
+const events = []
+
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    res.send('Hello There!')
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type Patient {
+            _id: ID!
+            title: String!
+            forname: String!
+            surname: String!
+            age: Int!
+        }
+
+        #
+        # Input is the required input for the create Mutations
+        #
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input PatientInput {
+            title: String!
+            forname: String!
+            surname: String!
+            age: Int!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+            patient: [Patient!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+            createPatient(patientInput: PatientInput): Patient
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: { 
+        events: () => {
+            return events
+        },
+
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+            events.push(event)
+            return event
+        }
+    },
+    
+    graphiql: true
+    })
+)
 
 app.listen(3000)
