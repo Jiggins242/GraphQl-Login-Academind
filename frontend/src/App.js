fbb77bccff34f4538a2aa8530816642bdef8079f
Jiggins242 <44429273+Jiggins242@users.noreply.github.com>
2019-01-10 11:17:17 +0000

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import EventsPage from './pages/Events';
 import BookingPage from './pages/Bookings';
 import MainNavigation from './components/Navigation/MainNavigation'
 
-import './App.css';
+import './styles/App.css';
 
 
 class App extends Component {
