6468952514e7870579c1de93dec6d9065a4e42b8
Jiggins242 <44429273+Jiggins242@users.noreply.github.com>
2019-01-10 10:41:19 +0000

    Basic navigation bar has been added in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 
+import AuthPage from './pages/Auth'
 import PatientsPage from './pages/Patients';
 import EventsPage from './pages/Events';
 import BookingPage from './pages/Bookings';
-import AuthPage from './pages/Auth'
+import MainNavigation from './components/Navigation/MainNavigation'
 
 import './App.css';
 
@@ -13,13 +14,18 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/patients" component={PatientsPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/patients" component={PatientsPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
